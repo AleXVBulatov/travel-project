8d1ad4ecfce4ad0948afb534865d1de780d2dc0b
Alexey Bulatov <bulatov.a.v.89@gmail.com>
2023-03-13 18:21:06 +0200

    Finished

--- js/main.js
+++ js/main.js
@@ -5,12 +5,30 @@ $(document).ready(function () {
 
 const owl = $(".owl-carousel");
 owl.owlCarousel({
-  items: 3, // показывать 3 слайда
-  margin: 30,
+  items: 1, // показывать 3 слайда
+  margin: 10,
   loop: true, // бесконечная прокрутка по кругу
   center: true,
   startPosition: 1, // чтобы слайдер начинался с первой позиции (2-ая картинка)
   // autoWidth: true, // чтобы размеры картинок были как указаны у меня
+  responsive: {
+    // breakpoint from 850 up
+    425: {
+      items: 2, // показывать 3 слайда
+    },
+
+    600: {
+      items: 3, // показывать 3 слайда
+    },
+    // breakpoint from 1024 up
+    1024: {
+      margin: 20,
+    },
+    // breakpoint from 1269 up
+    1269: {
+      margin: 30,
+    },
+  },
 });
 
 $(".slider__btn--prev").click(function () {
@@ -25,9 +43,21 @@ $(".slider__btn--next").click(function () {
 // вариант 1:
 const menuBtn = document.querySelector(".nav__btn-toggle");
 const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector(".nav");
+// const header = document.querySelector(".header");
+// const headerTop = document.getElementsByClassName("header__top")[0];
 
 menuBtn.addEventListener("click", () => {
   menuIcon.classList.toggle("js-menu-icon-active");
+  nav.classList.toggle("nav--mobile");
+  document.body.classList.toggle("no-scroll");
+  // if (nav.classList.contains("nav--mobile")) {
+  //   header.style.opacity = "0.5";
+  //   console.log(nav);
+  //   nav.children[0].style.cssText = `opacity: 1`;
+  // } else {
+  //   header.style.opacity = "1";
+  // }
 });
 
 // вариант 2:
